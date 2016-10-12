0008e2b1fcced0cc160a2e6a351bb7e3f4f0f76c
Lance Ball <lball@redhat.com>
2016-10-12 10:06:54 -0400

    Make CLI accept a configuration .json file
    Just pass the path to a json file to the `goldbot` executable, and it
    will merge those values with the existing defaults.

--- bgoldbot.js
+++ bgoldbot.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
 
+'use strict';
+
+const fs = require('fs');
 const GoldBot = require(`${__dirname}/index.js`);
-GoldBot.bot(GoldBot.defaults);
+
+let options = GoldBot.defaults;
+
+const configFile = process.argv[2];
+if (configFile) {
+  try {
+    console.log(`Reading configuration ${configFile}`);
+    const config = JSON.parse(fs.readFileSync(configFile).toString());
+    console.log('Using configuration');
+    console.log(config);
+    Object.assign(options, config);
+  } catch (e) {
+    console.error('Unable to read configuration json.', e);
+    console.error(e.stack);
+    process.exit();
+  }
+}
+GoldBot.bot(options);
