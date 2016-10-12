2f5d7a6924b4c00c78d660bfa60a53e14f72979f
Lance Ball <lball@redhat.com>
2016-10-11 23:13:28 -0400

    Initial commit

---
+++ bgoldbot.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+const GoldBot = require(`${__dirname}/index.js`);
+GoldBot.bot(GoldBot.defaults);
---
+++ index.js
@@ -0,0 +1,13 @@
+
+const bot = require('./lib/bot');
+
+const GoldBot = {
+  bot,
+  get defaults () {
+    // always return a fresh copy of the defaults
+    // so they can't be modified in user land
+    return Object.assign({}, bot.defaults);
+  }
+};
+
+module.exports = exports = GoldBot;
